b4f2d58eaff0387b9a066e0ed8b59e07c5acfc19
zhangtiantian10 <975571814@qq.com>
2016-08-18 11:22:01 +0800

    Add todo

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,56 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {createStore} from "redux";
+import reducer from './reducers/reducer';
+
+const store = createStore(reducer);
+
 
 class App extends React.Component {
+    add(text) {
+        store.dispatch({type:'ADD',text});
+    }
+
     render() {
         return <div>
-            hello
+            <TodoList todos={store.getState().todos}/>
+            <AddTodo onAdd={this.add}/>
         </div>
     }
 }
 
-render(<App/>, document.getElementById('root'));
+class TodoList extends React.Component {
+    render() {
+        return <div>
+            {this.props.todos.map(todo => {
+                return <div>{todo.text}</div>
+            })}
+        </div>
+    }
+}
+
+class AddTodo extends React.Component {
+    add() {
+        const text = this.refs.myInput.value;
+        this.props.onAdd(text);
+        this.refs.myInput.value = '';
+    }
+
+    render() {
+        return <div>
+            <input type="text" ref="myInput"/>
+            <button onClick={this.add.bind(this)}>添加</button>
+        </div>
+    }
+}
+
+
+function renderFunction() {
+    "use strict";
+    render(<App/>, document.getElementById('root'))
+}
+
+store.subscribe(renderFunction)
+
+renderFunction();
+
